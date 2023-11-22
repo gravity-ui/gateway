3ad36dacc9ccf9b3c4e5a15c885768a449441ea3
Vitaly Vasin <vitaly.n.vasin@gmail.com>
2023-11-22 13:03:37 +0100

    fix: auto remove old patches (#28)

--- bin/patch.js
+++ bin/patch.js
@@ -1,13 +1,12 @@
 #! /usr/bin/env node
 const {execSync} = require('child_process');
-const {copyFileSync, mkdirSync} = require('fs');
-mkdirSync('patches', {recursive: true});
-copyFileSync(
-    'node_modules/@gravity-ui/gateway/patches/grpc-reflection-js+0.3.0.patch',
-    'patches/grpc-reflection-js+0.3.0.patch',
-);
-copyFileSync(
-    'node_modules/@gravity-ui/gateway/patches/protobufjs+7.2.5.patch',
-    'patches/protobufjs+7.2.5.patch',
+const {copyFileSync, mkdirSync, readdirSync, rmSync} = require('fs');
+const srcPatchesDir = 'node_modules/@gravity-ui/gateway/patches';
+const dstPatchesDir = 'patches';
+
+mkdirSync(dstPatchesDir, {recursive: true});
+readdirSync(dstPatchesDir).forEach((patch) => rmSync(`${dstPatchesDir}/${patch}`));
+readdirSync(srcPatchesDir).forEach((patch) =>
+    copyFileSync(`${srcPatchesDir}/${patch}`, `${dstPatchesDir}/${patch}`),
 );
 execSync('npx patch-package', {stdio: 'inherit'});
