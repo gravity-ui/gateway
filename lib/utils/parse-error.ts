e8b9d8769214da5c50ff06abc5213d6fc1eaccad
Roman Fedorenkov <1071418+SeqviriouM@users.noreply.github.com>
2024-02-15 14:07:05 +0100

    fix!: move originalError to debug field (#39)

--- lib/utils/parse-error.ts
+++ lib/utils/parse-error.ts
@@ -16,10 +16,8 @@ export function parseMixedError(e: Error & {code?: string}) {
         status: 500,
         message: String(e.message || DEFAULT_GATEWAY_MESSAGE),
         code: String(e.code || DEFAULT_GATEWAY_CODE),
-        details: {
-            originalError: e,
-        },
         debug: {
+            originalError: e,
             stack: e.stack,
         },
     };
